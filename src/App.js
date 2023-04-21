573cd27fa218745b6612dd1d69b83325c681a499
kristish000 <kristish000@gmail.com>
2023-04-21 23:55:02 +0200

    currency

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = 'https://api.exchangeratesapi.io/latest'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const [currencyOptions,setCurrencyOptions]=useState([])
+const [fromCurrency,setFromCurrency]=useState()
+const [toCurrency,setToCurrency]=useState()
+  useEffect(()=>{
+      fetch(BASE_URL)
+        .then(res => res.json())
+        .then(data=>{
+          const firstcurrency = Object.keys(data.rates)[0]
+         setCurrencyOptions([data.base,...Object.keys(data.rates)])
+         setFromCurrency(data.base)
+         setToCurrency(firstcurrency)
+        })
+  }, [])
+
+  
+  
+
+  return (<>
+  <h1>Currency</h1>
+   <CurrencyRow 
+   currencyOptions={currencyOptions} 
+   selectedCurrency={fromCurrency}
+   onChangeCurrency={e=> setFromCurrency(e.target.value)}
+   />
+
+   <div className='equals'>=</div>
+   <CurrencyRow 
+   currencyOptions={currencyOptions}
+   selectedCurrency={toCurrency}
+   onChangeCurrency={e=> setToCurrency(e.target.value)}
+   />
+   </>
   );
 }
 
---
+++ src/CurrencyRow.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function CurrencyRow(props) {
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency
+
+  
+  }= props
+  return (
+    <div>
+      <input type='number' className='input'/>
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
+        {currencyOptions.map(option =>(
+          <option key={option}value={option}></option>
+
+        ))}
+        <option value='Hi'>Hi</option>
+      </select>
+    </div>
+  )
+}
